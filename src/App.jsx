75d92a9fcc9e92f410ed8749cd18576a7db52be2
Marcio <matiusbv@gmail.com>
2024-10-01 19:41:34 -0300

    1 Commit

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import { db } from './firebaseConnection'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import './App.css';
+
+
+function App() {
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [posts, setPosts] = useState([]);
+  const [idPost, setIdPost] = useState('')
+ 
+  async function handleAdd() {
+    await addDoc(collection(db, "Cadastro"), {
+      nome: nome,
+      telefone: telefone,
+    })
+    .then (() => {
+      console.log("CADASTRO COM SUCESSO")
+      setNome('');
+      setTelefone('')
+    })
+    .catch((error) => {
+      console.log("ERRO " + error)
+    })
+  }
+
+  async function buscarPost() {
+    const postsRef = collection(db, "Cadastro")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          nome: doc.data().nome,
+          telefone: doc.data().telefone,
+        })
+      })
+      setPosts(lista);
+    })
+    .catch((error) => {
+      console.log("Deu Algum erro ao buscar")
+    })
+  }
+
+  function editarPost() {
+    alert("oi")
+  }
+
+  return (
+    <div>
+    <h1>ReactJS + Firebase :)</h1>
+
+  <div className="container">
+    <label>Id do Post</label>
+    <input 
+      placeholder='Digite o ID do post'
+      value={idPost}
+      onChange={(e) => setIdPost(e.target.value)}
+    /><br/><br/>
+
+    <label>nome:</label>
+    <textarea 
+      type="text"
+      placeholder='Digite o nome'
+      value={nome}
+      onChange={ (e) => setNome(e.target.value) }
+    />
+
+    <label>telefone:</label>
+    <input 
+      type="text" 
+      placeholder="telefone do post"
+      value={telefone}
+      onChange={(e) => setTelefone(e.target.value) }
+    />
+
+    <button onClick={handleAdd}>Cadastrar</button>
+    <button onClick={buscarPost}>Buscar Posts</button>
+    <button onClick={editarPost}>Atualizar post</button>
+
+    <ul>
+      {posts.map( (post) => {
+        return(
+          <li key={post.id}>
+            <strong>ID: {post.id}</strong>
+            <span>Nome: {post.nome}</span>
+            <span>Telefone: {post.telefone}</span>
+          </li>
+        )
+      } )}
+    </ul>
+
+  </div>
+
+  </div>
+  );
+}
+
+export default App;
---
+++ src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeRONMaHC193ga_RlbortKmUbaBF_T4gQ",
+  authDomain: "rededehomens.firebaseapp.com",
+  projectId: "rededehomens",
+  storageBucket: "rededehomens.appspot.com",
+  messagingSenderId: "959004846660",
+  appId: "1:959004846660:web:599fb33eaab5d40903aa11",
+  measurementId: "G-T054LSP72P"
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseapp);
+
+export { db };
